167d08f5e0c884a181d3ad17f6defce41a3278b9
Sachin Jadhav <sachinjadhav_123@yahoo.com>
2021-11-04 19:02:19 +0530

    create event editor fix

--- client/src/pages/CreateEvent/TinyMceEditor.js
+++ client/src/pages/CreateEvent/TinyMceEditor.js
@@ -1,124 +1,88 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
-import { Global, css } from "@emotion/core";
-import CKEditor from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import CKEditor from "react-ckeditor-component";
 
 class TinyMceEditor extends Component {
   constructor(props) {
     super(props);
-    this.onChange = this.onChange.bind(this);
-  }
-  ckWrapperStyle = css`
-    position: relative;
-    z-index: 1;
-    &::before {
-      color: rgba(192, 192, 192, 1);
-      content: attr(data-placeholder);
-      padding: 0 11px;
-      position: absolute;
-      margin: var(--ck-spacing-large) 0;
-      top: 0;
-      z-index: -1;
-    }
-  `;
+    this.updateContent = this.updateContent.bind(this);
 
-  onChange(e) {
-    console.log(e.target.getContent());
+    this.state = {
+      content: "",
+    };
   }
+
+  updateContent = (newContent) => {
+    this.setState({
+      content: newContent,
+    });
+  };
+
+  onChange = (evt) => {
+    var newContent = evt.editor.getData();
+    this.setState({
+      content: evt.editor.getData(),
+    });
+    console.log(this.state.content);
+  };
+
+  onBlur = (evt) => {
+    // console.log("onBlur event called with event info: ", evt);
+  };
+
+  afterPaste = (evt) => {
+    // console.log("afterPaste event called with event info: ", evt);
+  };
 
   render() {
+    let config = {
+      toolbarGroups: [
+        { name: "document", groups: ["mode", "document", "doctools"] },
+        {
+          name: "editing",
+          groups: ["find", "selection", "spellchecker", "editing"],
+        },
+        { name: "forms", groups: ["forms"] },
+        { name: "basicstyles", groups: ["basicstyles", "cleanup"] },
+        {
+          name: "paragraph",
+          groups: ["list", "indent", "blocks", "align", "bidi", "paragraph"],
+        },
+
+        { name: "links", groups: ["links"] },
+        { name: "insert", groups: ["insert"] },
+        { name: "styles", groups: ["styles"] },
+        { name: "colors", groups: ["colors"] },
+        { name: "tools", groups: ["tools"] },
+        "/",
+        { name: "clipboard", groups: ["clipboard", "undo"] },
+        { name: "others", groups: ["others"] },
+        { name: "about", groups: ["about"] },
+      ],
+      removeButtons:
+        "Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,SelectAll,Scayt,Replace,Form,Checkbox,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,BidiLtr,BidiRtl,Language,Flash,Smiley,SpecialChar,PageBreak,Iframe,Anchor,ShowBlocks,About,CopyFormatting,Undo,Redo",
+      fontSize_sizes: "16/16px;24/24px;48/48px;",
+      font_names:
+        "Arial/Arial, Helvetica, sans-serif;" +
+        "Times New Roman/Times New Roman, Times, serif;" +
+        "Verdana",
+      allowedContent: true,
+      // disableNativeSpellChecker: false
+      // skin: "moono",
+      // plugins:
+      //   "dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,copyformatting,div,resize,elementspath,enterkey,entities,popup,filetools,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,tableselection,undo,lineutils,widgetselection,widget,notificationaggregator,uploadwidget,uploadimage,wsc"
+    };
     return (
-      <div
-        className="ckeditor-wrapper"
-        css={css`
-          ${this.ckWrapperStyle}
-        `}
-      >
-        <Global
-          styles={css`
-            :root {
-              --ck-border-radius: 4px;
-              --ck-color-focus-border: rgba(96, 103, 113, 0.8);
-              --ck-color-shadow-inner: rgba(69, 79, 99, 0.2);
-              --ck-inner-shadow: 0 0 0 2px var(--ck-color-shadow-inner);
-              --ck-spacing-large: var(--ck-spacing-standard);
-            }
-            .ck.ck-editor__editable_inline {
-              border: 1px solid rgba(217, 217, 217, 1);
-              transition: all 0.3s;
-              &:hover {
-                border-color: rgba(96, 102, 112, 1);
-                border-right-width: 1px !important;
-              }
-            }
-            .ck-editor__editable.ck-read-only {
-              background-color: rgba(245, 245, 245, 1);
-              opacity: 1;
-              cursor: not-allowed;
-              color: rgba(0, 0, 0, 0.25);
-              &:hover {
-                border-color: rgba(217, 217, 217, 1);
-              }
-            }
-          `}
-        />
-        <CKEditor
-          editor={ClassicEditor}
-          data="<p><p>Type description here !</p></p>"
-          config={{
-            toolbar: [
-              "heading",
-              "|",
-              "bold",
-              "italic",
-              "link",
-              "bulletedList",
-              "numberedList",
-              "blockQuote",
-              "ckfinder",
-              "|",
-              "imageTextAlternative",
-              "imageUpload",
-              "imageStyle:full",
-              "imageStyle:side",
-              "|",
-              "mediaEmbed",
-              "insertTable",
-              "tableColumn",
-              "tableRow",
-              "mergeTableCells",
-              "|",
-              "undo",
-              "redo",
-            ],
-          }}
-          onInit={(editor) => {
-            // You can store the "editor" and use when it is needed.
-            console.log("Editor is ready to use!", editor);
-            // console.log(
-            //   "toolbar: ",
-            //   Array.from(editor.ui.componentFactory.names())
-            // );
-            // console.log(
-            //   "plugins: ",
-            //   ClassicEditor.builtinPlugins.map((plugin) => plugin.pluginName)
-            // );
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(editor) => {
-            console.log("Blur.", editor);
-          }}
-          onFocus={(editor) => {
-            console.log("Focus.", editor);
-          }}
-        />
-      </div>
+      <CKEditor
+        data="<p><p>Type description here !</p></p>"
+        config={config}
+        content={this.state.content}
+        events={{
+          blur: this.onBlur,
+          afterPaste: this.afterPaste,
+          change: this.onChange,
+        }}
+      />
     );
   }
 }
-
 export default TinyMceEditor;
